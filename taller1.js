4d9034f8122a76dbc12cb7bf2171d28a252348ef
anyelot <81717749+anyelot@users.noreply.github.com>
2025-02-14 15:01:50 -0500

    taller1.js

--- taller1.js
+++ taller1.js
@@ -4,85 +4,23 @@ function convertidorTemp(C){
 }
 
 //Punto 2
-function resolvedor(a, b, c, signo){
-    const x = Math.sqrt(b * b - 4 * a * c)
-    if (signo == "+"){
-        return (-b + x) / (2 * a)
-    }else{
-        return (-b - x) / (2 * a)
-    }   
+function resolvedor(a, b, c, positivo=true){
+    let discriminante = Math.sqrt(b * b - 4 * a * c);
+    return positivo ? (-b + discriminante) / (2 * a) : (-b - discriminante) / (2 * a);
 }
 
 //Punto 3
 function mejorParidad(num){
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
+    return num % 2 === 0;
 }
 
 
 //Punto 4
 function peorParidad(num){
-    num = 1
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 2
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 3
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 4
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 5
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 6
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 7
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 8
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 9
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
-    }
-    num = 10
-    if (num % 2 == 0){
-        return true
-    }else{
-        return false
+    switch(num) {
+        case 2: case 4: case 6: case 8: case 10:
+            return true;
+        default:
+            return false;
     }
 }
